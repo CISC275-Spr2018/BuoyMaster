785757218909c29951eab37132bc793b221af1b2
evancaplan <ecaplan@udel.edu>
2018-04-22 13:55:35 -0400

    Created a timer for the view
    With 10 Seconds left it doubles in size as a warning

---
+++ timerListener.java
@@ -0,0 +1,24 @@
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Timer;
+import javax.swing.JLabel;
+public class timerListener implements ActionListener {
+	private static int countdown=100;
+	public static JLabel label = new JLabel(Integer.toString(countdown));
+	Timer timer = new Timer();
+	public int getTime(){
+		return this.countdown;
+	}
+	public void setCoundown(int x){
+		this.countdown+=x;
+	}
+	@Override 
+	public void actionPerformed(ActionEvent e){
+		label.setText(Integer.toString(countdown--));
+		if (countdown==10){
+			label.setFont(new Font(label.getFont().getFontName(),Font.PLAIN,label.getFont().getSize()*2));
+		}
+	}
+	
+}
