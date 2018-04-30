baa920b18a9fae2db668f7c223cf5ed9e5986ca3
Evan Caplan <evancaplan@wifi-roaming-128-4-145-83.host.udel.edu>
2018-04-30 18:35:32 -0400

    adding screens and choices

--- Controller.java
+++ Controller.java
@@ -1,4 +1,4 @@
-import java.awt.EventQueue;
+import java.awt.EventQueue; 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -6,29 +6,38 @@ import javax.swing.Timer;
 
 public class Controller implements ActionListener {
 	private final static int DRAWDELAY = 50;
-	Model model;
-	EstuaryView EView;
-	
+	static Model model;
+	static EstuaryView EView2;
+	static EstuaryView EView;
+	static boolean start=false;
 	public Controller(){
 		model = new Model();
 		EView = new EstuaryView(model);
-		
 		Timer t = new Timer(DRAWDELAY, this);
 		t.start();
+		
+		
 	}
-
 	@Override
+	/*@param takes in an ActionEvent arg0
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
 	public void actionPerformed(ActionEvent arg0) {
+		if(start)
 		model.modelUpdate();
 		EView.update();
 	}
 	
+	
 	public static void main(String[] args){
+		
 		EventQueue.invokeLater(new Runnable(){
 			public void run(){
 				Controller c = new Controller();
 				c.EView.addKeyListener(c.model);
-			}
+				}
+			
 		});
 	}
 }
+	
--- EstuaryView.java
+++ EstuaryView.java
@@ -1,6 +1,20 @@
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
 /*
@@ -13,34 +27,133 @@ public class EstuaryView extends JPanel{
 	JFrame frame;
 	int xLoc;
 	int yLoc;
-	
+	JPanel panel= new JPanel();
+	JPanel startPanel = new JPanel(new BorderLayout());
+	JPanel selectionPanel = new JPanel(new BorderLayout());
+	JPanel estuaryPanel;
 	PlayerPanel playerPanel;
 	BuoyPanel buoyPanel;
 	TimerPanel timerPanel;
 	GameMessagePanel gameMessagePanel;
 	SandBarPanel sandBarPanel;
 	DockPanel dockPanel;
+	CardLayout cl= new CardLayout();
+	Controller c;
 	
 	EstuaryView(Model model){
+		panel.setLayout(cl);
+		//create start panel stuff
+		//startPanel.setSize(model.width, model.height);
+		JButton button1 = new JButton("Start");
+	    button1.setSize(20,20);
+	    button1.setVisible(true);
+	    button1.setActionCommand("start");
+		BufferedImage logo=null;
+		try{
+			logo=ImageIO.read(new File("images/logo.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		JLabel picLabel= new JLabel(new ImageIcon(logo));
+		startPanel.add(button1,BorderLayout.SOUTH);
+		startPanel.add(picLabel, BorderLayout.CENTER);
+		startPanel.setBackground(Color.BLUE.darker().darker());
+		button1.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0){
+				cl.show(panel,"2");
+			}
+		});
+		//stuff for selection panel
+		//selectionPanel.setSize(model.width, model.height);
+		BufferedImage choice1 = null;
+		BufferedImage choice2=null;
+		BufferedImage choice3=null;
+		try {
+			choice1 = ImageIO.read(new File("images/boat_east.png"));
+			choice2=ImageIO.read(new File("images/boat_east.png"));
+			choice3= ImageIO.read(new File("images/boat_east.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JButton button2 = new JButton(new ImageIcon(choice1));
+		button2.setBorder(BorderFactory.createEmptyBorder());
+		button2.setContentAreaFilled(false);
+	    button2.setSize(20,20);
+	    button2.setVisible(true);
+	    button2.setActionCommand("select");
+		button2.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0){
+				cl.show(panel, "3");
+				c.start=true;
+		
+			}
+		});
+		
+		JButton button3 = new JButton(new ImageIcon(choice2));
+		button3.setBorder(BorderFactory.createEmptyBorder());
+		button3.setContentAreaFilled(false);
+	    button3.setSize(20,20);
+	    button3.setVisible(true);
+	    button3.setActionCommand("select");
+		button3.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0){
+				cl.show(panel, "3");
+				c.start=true;
+		
+			}
+		});
+		
+		JButton button4 = new JButton(new ImageIcon(choice3));
+		button4.setBorder(BorderFactory.createEmptyBorder());
+		button4.setContentAreaFilled(false);
+	    button4.setSize(20,20);
+	    button4.setVisible(true);
+	    button4.setActionCommand("select");
+		button4.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0){
+				cl.show(panel, "3");
+				c.start=true;
+		
+			}
+		});
+		
+	    selectionPanel.add(button2,BorderLayout.NORTH);
+	    selectionPanel.add(button3,BorderLayout.CENTER);
+	    selectionPanel.add(button4,BorderLayout.SOUTH);
+	    selectionPanel.setBackground(Color.BLUE.darker().darker());
+	    panel.add(startPanel, "1");
+	    panel.add(selectionPanel, "2");
+	   
 		playerPanel = new PlayerPanel(model.player);
 		buoyPanel = new BuoyPanel(model.buoy);
 		timerPanel = new TimerPanel(model.timer);
 		gameMessagePanel = new GameMessagePanel(model.gameMessage);
 		sandBarPanel = new SandBarPanel(model.sandBar);
 		dockPanel = new DockPanel(model.dock);
-		
-		setFocusable(true);
-		
+		estuaryPanel=this;
 		this.frame = new JFrame();
-		this.frame.add(this);
-		
-		this.setBackground(Color.BLUE);
-		
-		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.frame.setSize(model.width, model.height);
+		this.requestFocus();
+		this.frame.setBackground(Color.BLUE.darker().darker());
+		this.setBackground(Color.BLUE.darker().darker());
+		this.frame.setSize(1000,5000);
 		this.frame.setVisible(true);
+		panel.add(estuaryPanel, "3");
+		panel.requestFocusInWindow();
+		this.frame.add(panel);
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 	}
-	
+	public void changeFromStart() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	public void update(){
 		this.repaint();
@@ -57,5 +170,8 @@ public class EstuaryView extends JPanel{
 		dockPanel.paintComponent(g);
 		
     }
+
+
+	
 	
 }
