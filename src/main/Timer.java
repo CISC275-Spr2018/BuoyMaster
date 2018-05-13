36b9877f3a7a10c797ea02601b8a0a13ddc4a6d4
Evan Caplan <evancaplan@Evans-MBP.fios-router.home>
2018-05-13 13:20:40 -0400

    started to experiment with timer graphic

--- src/main/Timer.java
+++ src/main/Timer.java
@@ -1,5 +1,5 @@
 package main;
-/*@author Arvin Aya-ay, Greg White, Evan Caplan, Riley Shaw, Dan Hinrichs 
+/**@author Arvin Aya-ay, Greg White, Evan Caplan, Riley Shaw, Dan Hinrichs 
  * 
  */
 public class Timer {
@@ -14,9 +14,6 @@ public class Timer {
 		timeLeft = 500; //THIS HAS TO BE CHANGED TO A BETTER TIMER!!! Right now it just counts down the number of times the modelUpdate method is called. It should count down in real time milliseconds.
 		decrement = 1;
 		message = "";
-	}
-	void increment(){
-		timeLeft+=30;
 	}
 	/*Updates the timer as time goes on by decrementing.
 	 * 
