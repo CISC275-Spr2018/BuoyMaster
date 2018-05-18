44ff764313c7df186b63682826257d2ef0d170e3
Arvin Aya-ay <arvinayaay98@gmail.com>
2018-05-18 18:06:05 -0400

    press s to save and l to load

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -2,6 +2,8 @@ package main;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -19,7 +21,7 @@ import javax.swing.Timer;
 /**@author Arvin Aya-ay, Greg White, Evan Caplan, Riley Shaw, Dan Hinrichs 
  * 
  */
-public class Controller implements ActionListener, Serializable{
+public class Controller implements ActionListener, Serializable, KeyListener{
 	private final int width = 1280;
 	private final int height = 720;
 	private final static int DRAWDELAY = 50;
@@ -53,6 +55,9 @@ public class Controller implements ActionListener, Serializable{
 	public View getView(){
 		return view;
 	}
+	public void setModel(Model m) {
+		this.model = m;
+	}
 	/**
 	 * (non-Javadoc)
 	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
@@ -70,7 +75,7 @@ public class Controller implements ActionListener, Serializable{
 			model.setVessel(new SpeedBoat());
 		}
 
-		gkl = new GameKeyListener(model.getPlayer(), model);
+		gkl = new GameKeyListener(this);
 		view.addKeyListener(gkl);
 		start = true;
 	}
@@ -172,7 +177,7 @@ public class Controller implements ActionListener, Serializable{
 							model.setVessel(new FishingBoat());
 							model.tutorial=false;
 
-							gkl = new GameKeyListener(model.getPlayer(), model);
+							gkl = new GameKeyListener(this);
 							view.addKeyListener(gkl);
 							view.estuaryScreen.getTimerPanel().timerLength=500;
 							answered=false;
@@ -219,7 +224,7 @@ public class Controller implements ActionListener, Serializable{
 							model.getBuoy().rand=random.nextInt(14);
 							model.tutorial=false;
 
-							gkl = new GameKeyListener(model.getPlayer(), model);
+							gkl = new GameKeyListener(this);
 							view.addKeyListener(gkl);
 							view.estuaryScreen.getTimerPanel().timerLength=500;
 							answered=false;
@@ -256,5 +261,20 @@ public class Controller implements ActionListener, Serializable{
 				t.start();
 			}
 		});
+	}
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
